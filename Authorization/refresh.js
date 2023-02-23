39391f9cfdceac9ed1d4eb8e2495b16070406d5a
Patrick Marmion <patrickmarmion@patrick.marmion>
2023-02-23 08:31:05 +0000

    refresh workflow

--- Authorization/refresh.js
+++ Authorization/refresh.js
@@ -1,29 +1,31 @@
 const fs = require('fs');
 const axiosInstance = require("../Config/axiosInstance");
 
+
+const credentialsPath = '../Credentials/credentials.json';
+const refreshPath = '../Credentials/refresh.json';
+const tokenPath = '../Credentials/token.json';
+
 const refreshAccessToken = async () => {
-    let cred = fs.readFileSync('../Credentials/credentials.json');
-    let creds = JSON.parse(cred);
-    let rawdata = fs.readFileSync('../Credentials/refresh.json');
-    let files = JSON.parse(rawdata);
-    const refresh_token = files.refresh_token;
-    const client_id = creds.installed.client_id;
-    const client_secret = creds.installed.client_secret;
-    let body = {
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "refresh_token": refresh_token,
-        "grant_type": "refresh_token"
-    }
-    let response = await axiosInstance.post('https://www.googleapis.com/oauth2/v4/token', body);
-    let accessToken = response.data.access_token;
-    const fileName = '../Credentials/token.json';
-    const file = require(fileName);
-    file.access_token = accessToken;
-    fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
-        if (err) return console.log(err);
-        console.log(JSON.stringify(file));
-    });
+  const credentials = JSON.parse(fs.readFileSync(credentialsPath));
+  const { client_id, client_secret } = credentials.web;
+  const { refresh_token } = JSON.parse(fs.readFileSync(refreshPath));
+  const body = {
+    client_id,
+    client_secret,
+    refresh_token,
+    grant_type: 'refresh_token',
+  };
+  try {
+    const response = await axiosInstance.post('https://www.googleapis.com/oauth2/v4/token', body);
+    const accessToken = response.data.access_token;
+    const tokenFile = JSON.parse(fs.readFileSync(tokenPath));
+    tokenFile.access_token = accessToken;
+    fs.writeFileSync(tokenPath, JSON.stringify(tokenFile));
+    console.log(JSON.stringify(tokenFile));
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = refreshAccessToken;
